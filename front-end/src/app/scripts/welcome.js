c91315fab6c5e337d08feb39d3ad75d18eca4c8a
Evan <evan.chinnaya@epfedu.fr>
2024-03-05 11:15:04 +0100

    CSS Loader

--- front-end/src/app/scripts/welcome.js
+++ front-end/src/app/scripts/welcome.js
@@ -1,23 +1,23 @@
 // TODO #import-html: use ES default imports to import welcome.html as template
 // TODO #export-functions: remove the IIFE
-(function () {
-  // TODO #export-functions: export function WelcomeComponent
-  // TODO #class: use the ES6 class keyword
-  /* class WelcomeComponent constructor  */
-  function WelcomeComponent() {
-    // TODO #extends: call super(template)
-    // TODO #import-html: assign template to this.template
-  }
 
-  // TODO #export-functions: remove this line
-  // put component in global scope, to be runnable right from the HTML.
-  window.WelcomeComponent = WelcomeComponent;
-  // TODO #class: turn function into a method of WelcomeComponent
-  /* method WelcomeComponent.init */
-  WelcomeComponent.prototype.init = function init() {
-    var form = document.querySelector("form.form-signin");
+// TODO #export-functions: export function WelcomeComponent
+// TODO #class: use the ES6 class keyword
+/* class WelcomeComponent constructor  */
+export function WelcomeComponent() {
+  // TODO #extends: call super(template)
+  // TODO #import-html: assign template to this.template
+}
 
-    form.addEventListener(
+// TODO #export-functions: remove this line
+// put component in global scope, to be runnable right from the HTML.
+window.WelcomeComponent = WelcomeComponent;
+// TODO #class: turn function into a method of WelcomeComponent
+/* method WelcomeComponent.init */
+WelcomeComponent.prototype.init = function init() {
+  var form = document.querySelector("form.form-signin");
+
+  form.addEventListener(
       "submit",
       // TODO #arrow-function: use arrow function instead.
       function (event) {
@@ -33,16 +33,15 @@
         }
       }.bind(this),
       false
-    );
+  );
 
-    return this;
-  };
+  return this;
+};
 
-  // TODO #class: turn function into a method of WelcomeComponent
-  function _startGame(name, size) {
-    // TODO #spa: replace with './#game'
-    var gamePage = "./#game";
-    // TODO #template-literals:  use template literals (backquotes)
-    window.location = gamePage + "?name=" + name + "&size=" + size;
-  }
-})();
+// TODO #class: turn function into a method of WelcomeComponent
+function _startGame(name, size) {
+  // TODO #spa: replace with './#game'
+  var gamePage = "./#game";
+  // TODO #template-literals:  use template literals (backquotes)
+  window.location = gamePage + "?name=" + name + "&size=" + size;
+}
